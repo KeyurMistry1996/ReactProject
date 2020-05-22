27ff78e2932a21676d9e09c90ab61a0cf7250fbb
keyurmistry <keyurmistry96@gmail.com>
2020-05-22 10:49:47 -0400

    web address changed

--- src/components/Reusable/Cart/Coursecart.js
+++ src/components/Reusable/Cart/Coursecart.js
@@ -36,7 +36,7 @@ export default class Coursecart extends Component {
                       data-item-id={node.id}
                       data-item-name={node.title}
                       data-item-price={node.price}
-                      data-item-url="http://learncodeonlone.in"
+                      data-item-url="https://reactprojectlco.netlify.app/"
                       data-item-image={node.image.fixed.src}
                     >
                       Join Now
